2cafc786d3fa4587df85ee09df27281c505b8643
Xiao Hu <xiaohu@pingidentity.com>
2022-06-13 18:14:40 -0600

    Implement two number operations

--- script.js
+++ script.js
@@ -19,43 +19,71 @@ function operate(operator, a, b) {
     return operator(a, b);
 }
 
-//Display Numbers
 
+
+//Display Numbers
 let num = document.querySelectorAll('.num');
 const display = document.getElementById('display-num');
-let displayValue1 = [];
-let displayValue2 = [];
+let displayValue = [];
+let firstNum = [];
+let secondNum = [];
 
 num.forEach((key) => {
   key.addEventListener('click', () => {
     const number = parseInt(key.textContent);
-    displayValue1.push(number)
-    display.textContent = displayValue1.join().replace(/,/g, '');
+    displayValue.push(number);
+    display.textContent = displayValue.join().replace(/,/g, '');
   });
 });
+
+
 
 //Perform Operations
 let operators = document.querySelectorAll('.operator');
 let currentOperator = [];
+let operatorFunction;
 
 operators.forEach((key) => {
   key.addEventListener('click', () => {
     const operator = key.textContent;
-    if (operator === 'x') currentOperator.push(multiply);
-    if (operator === '÷') currentOperator.push(divide);
-    if (operator === '-') currentOperator.push(add);
-    if (operator === '+') currentOperator.push(subtract);
-    display.textContent = "";
-    // displayValue.push('split');
+    if (operator === 'x') operatorFunction = multiply;
+    if (operator === '÷') operatorFunction = divide;
+    if (operator === '-') operatorFunction = subtract;
+    if (operator === '+') operatorFunction = add;
+    currentOperator = operator;
+    displayValue.push(` ${operator} `);
+    display.textContent = displayValue.join().replace(/,/g, '');
+
+    //check if there's already a total in display
+    
+    
   });
 });
 
 const equal = document.getElementById('equal')
+const displayTotal = document.getElementById('total');
 
 equal.addEventListener('click', () => {
+  firstNum = parseInt(displayValue.splice(0, displayValue.indexOf(` ${currentOperator} `)).join().replace(/,/g, ''));
+  secondNum = parseInt(displayValue.splice(1).join().replace(/,/g, ''));
+  let total = operate(operatorFunction, firstNum, secondNum);
+  displayValue = [];
+  displayTotal.textContent = total;
 
 }
-)
+);
+
+
+
+//Clear
+const clear = document.getElementById('clear');
+
+clear.addEventListener('click', () => {
+  displayValue = [];
+  display.textContent = "";
+  displayTotal.textContent = "";
+
+});
 
 
   
